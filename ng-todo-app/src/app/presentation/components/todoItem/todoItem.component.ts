b0a04fa3df4902cd6e79974e7b6ed7494997c020
Falk <falk.ba@gmx.de>
2021-09-28 15:51:18 +0200

    remove unused imports

--- ng-todo-app/src/app/presentation/components/todoItem/todoItem.component.ts
+++ ng-todo-app/src/app/presentation/components/todoItem/todoItem.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { Store } from '@ngxs/store';
 import { Todo } from 'src/app/features/todo-list/domain/entities/todo';
 
 @Component({
